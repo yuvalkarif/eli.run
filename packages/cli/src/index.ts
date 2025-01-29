601a600e7885c517c21d74223030461ce3844d18
Yuval Karif <yuvalkarif3@gmail.com>
2025-01-29 22:52:34 +0200

    fix: fix cli behaviour

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,33 +1,46 @@
-import { defineCommand, runMain } from 'citty';
-import open from 'open';
-import pc from 'picocolors';
+import { defineCommand, runMain } from "citty";
+import open from "open";
+import pc from "picocolors";
+import ora from "ora";
 
 const main = defineCommand({
   meta: {
-    name: 'eli',
-    version: '0.1.0',
-    description: 'Open links directly from your terminal'
+    name: "eli",
+    version: "0.1.0",
+    description: "Open links directly from your terminal",
   },
   args: {
-    terms: {
-      type: 'positional',
-      description: 'Search terms',
+    run: {
+      type: "positional",
+      description: "what to run",
       required: true,
-      array: true
-    }
+      default: "run",
+    },
   },
   async run({ args }) {
-    const searchTerms = args.terms
-    const url = 'https://g.co/kgs/PX7HS6f';
-    
+    const searchTerms =
+      typeof args.run === "string" ? [args.run, ...args._].join(" ") : "run";
+    const url = "https://shorturl.at/lQUot";
+
     try {
+      await mockAwait(`Searching for ${pc.blue(searchTerms)}...`);
       await open(url);
-      console.log(pc.green('✨'), pc.bold(`Searching for ${pc.blue(searchTerms)}...`));
     } catch (error) {
       console.error(pc.red(`Failed to open browser:`), error);
       process.exit(1);
     }
-  }
+  },
 });
 
 runMain(main);
+
+async function mockAwait(text: string) {
+  const spinner = ora({
+    text,
+    spinner: "dots", // You can choose any spinner from cli-spinners
+  }).start();
+
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  spinner.succeed("Wallak, there you go!");
+}
